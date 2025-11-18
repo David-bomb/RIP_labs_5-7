8d9f28973f563c8cf26f54db2d1a50352fd98b78
David-bomb <kot.shubin@mail.ru>
2025-11-18 22:41:58 +0300

    before initializing

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.gc5gol6kgl8"
+    "revision": "0.lg6v7rtj17o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
